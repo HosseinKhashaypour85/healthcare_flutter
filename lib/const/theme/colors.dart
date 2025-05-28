ca5239dd061d8bf97170d9d49e3f5ca1525045f5
hosseinkhashaypour <h20062006p@gmail.com>
2025-05-28 23:19:16 +0330

    many changes

---
+++ lib/const/theme/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFFffa600);
+const primary2Color = Color(0xFF292929);
+const primary3Color = Color(0xFF0026EB);
+const linkColor = Color(0xFF0094FF);
+const buttonColor = Color(0xFFFFB800);
+const boxColors = Color(0xFFADADAD);
+const box2Colors = Color(0xFF4cb050);
+const box3Colors = Color(0xFF1cb562);
+const box4Colors = Color(0xFF19ab56);
+const box5Colors = Color(0xFF682b00);
+const textColors = Color(0xFF272727);
+const darkColor = Color(0xFF181818);
+const textFieldColor = Color(0xFFF1F2F4);
+const backGroundColor = Color(0xFFFFFFFF);
+const buttonBgColor = Color(0xFF00E115);
+// const backGroundColor = Color(0xFFF5F5F5);
+
+const iconColor = Color(0xffffa600);
+const whiteColor = Colors.white;
+final greyColor = Colors.grey.shade300;
